aab964d9d8a210a7a883e71fab70b9f6949e7aa3
mervereis <mervereis78@gmail.com>
2026-09-24 21:32:03 +0100

    Revert "Add validation for bloom content length in send_bloom function"
    This reverts commit 97df7db715937bbc9148fc7ef6fc36982e4ff8be.

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -157,15 +157,6 @@ def send_bloom():
         return type_check_error
 
     user = get_current_user()
-    content = request.json["content"]
-    MAX_BLOOM_LENGTH = 280
-    if len(content) > MAX_BLOOM_LENGTH:
-        return make_response(
-            (
-                {"success": False, "message": f"Bloom cannot be longer than {MAX_BLOOM_LENGTH} characters"},
-                400,
-            )
-        )
 
     blooms.add_bloom(sender=user, content=request.json["content"])
 
